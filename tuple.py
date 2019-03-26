33f7831d9719e737cb8ae2592411bd974dadd466
ashish717744 <45986370+ashish717744@users.noreply.github.com>
2019-03-26 15:44:41 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,9 @@
+exam_date =(23,3,12)
+i =exam_date[0]
+j =exam_date[1]
+k =exam_date[2]
+print('The examination will start from :',i,'/',j,'/',k,end=' ')
+
+or
+exam_st_date = (11,12,2014)
+print( "The examination will start from : %i / %i / %i"%exam_st_date)
