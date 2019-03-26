e2796867ad14fce8c0e6f73e951186364bb8116b
ashish717744 <45986370+ashish717744@users.noreply.github.com>
2019-03-26 16:16:35 +0530

    Add files via upload

---
+++ mindblowing.py
@@ -0,0 +1,9 @@
+#Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn. Go to the editor
+#Sample value of n is 5 
+#Expected Result : 615
+
+a = int(input("Input an integer : "))
+n1 = int( "%s" % a )
+n2 = int( "%s%s" % (a,a) )
+n3 = int( "%s%s%s" % (a,a,a) )
+print (n1+n2+n3)
