880b2921e1a8c95cc8290f5ef679c3ace95c0811
ashish717744 <45986370+ashish717744@users.noreply.github.com>
2019-03-27 14:40:09 +0530

    Add files via upload

---
+++ charcterinput.py
@@ -0,0 +1,9 @@
+#Write a Python program to test whether a passed letter is a vowel or not
+list =['a','e','i','o','u']
+user =str(input('enter the character'))
+
+if user in list:
+    print('its a vowel')
+else:
+    print('not a vowel')
+    
