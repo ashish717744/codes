235edc5a47293fc5d7a26bca6bb7c22323c35c62
ashish717744 <45986370+ashish717744@users.noreply.github.com>
2019-03-27 13:37:09 +0530

    Add files via upload

---
+++ twodatesubtractioncode.py
@@ -0,0 +1,13 @@
+
+
+#Write a Python program to calculate number of days between two dates.
+#Sample dates : (2014, 7, 2), (2014, 7, 11)
+#Expected output : 9 days #
+
+
+import datetime
+max = datetime.date(2014, 7, 11)
+  
+min = datetime.date(2014, 7, 2)
+print(max-min)
+#there is help function which will that what a function you just have to import that function and say help(thatfunction)
