7be5b49460f0fd18e9687afe7417e2f2585a7458
ashish717744 <45986370+ashish717744@users.noreply.github.com>
2019-04-01 20:28:33 +0530

    Add files via upload

---
+++ typeofdatatype.py
@@ -0,0 +1,8 @@
+#Write a Python program to add two objects if both objects are an integer type. G
+n =input('')
+n1 =input('')
+if type(n)=='int' and type(n1)=='int':
+   sum =n+n1
+   print(sum)
+else:
+   print('cant print')
