91b8e4bb50fb0f7e4a70029ee45b2e000868e61d
ashish717744 <45986370+ashish717744@users.noreply.github.com>
2019-04-03 17:59:12 +0530

    Add files via upload

---
+++ lenofstring.py
@@ -0,0 +1,6 @@
+#. Write a Python program to calculate the length of a string. Go to the editor
+#Click me to see the sample solution
+string = input('enter the string')
+
+length =len(string)
+print(length)
